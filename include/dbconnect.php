4df49b997da56158ba04cf36eb052577b7220ce1
Nael-Sayegh <naelousay@gmail.com>
2025-06-15 14:06:38 +0200

    fixe name $bdd2 in dbconnect.php

--- include/dbconnect.php
+++ include/dbconnect.php
@@ -9,18 +9,18 @@ try
 }
 catch (PDOException $e)
 {
-    print 'Erreur de connexion à la base de données';
+    print 'Erreur de connexion à la base de données 1';
     error_log('DB connect error: '.$e->getMessage());
 }
 
 try
 {
-    $bdd = new PDO(DB_STRING2, DB_USER2, DB_PSW2);
+    $bdd2 = new PDO(DB_STRING2, DB_USER2, DB_PSW2);
     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e)
 {
-    print 'Erreur de connexion à la base de données';
+    print 'Erreur de connexion à la base de données 2';
     error_log('DB connect error: '.$e->getMessage());
 }
 

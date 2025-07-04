4072aed6a2dae741dffe1fbc94ba46b2e31309cd
Nael-Sayegh <naelousay@gmail.com>
2025-07-04 18:50:40 +0200

    Remove english translation

--- locales/en/confirm.tr.php
+++ locales/en/confirm.tr.php
@@ -4,7 +4,7 @@ $tr = [
   '_' => 'confirm',
   '_todo_level' => 0,
   '_last_author' => 'Nael Sayegh',
-  '_last_modif' => 1751646532,
+  '_last_modif' => 1751647840,
   'mail_info_subject' => 'Your member information',
   'mail_info_body_html' => <<<HTML
       <h2>Hello {{username}} and welcome to the {{site}} community</h2>

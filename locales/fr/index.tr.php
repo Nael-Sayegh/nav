779261c341c51b8c59a68d5158e822d177d7b828
Nael-Sayegh <naelousay@gmail.com>
2025-07-05 16:06:00 +0200

    Update website presentation

--- locales/fr/index.tr.php
+++ locales/fr/index.tr.php
@@ -4,19 +4,20 @@ $tr = [
   '_' => 'index',
   '_todo_level' => 0,
   '_last_author' => 'Pascal',
-  '_last_modif' => 1748185111,
+  '_last_modif' => 1751724360,
   'title' => '{{site}}&nbsp;: {{slogan}}',
   'happynewyear' => '<p role="alert">Toute l\'équipe {{site}} vous souhaite une excellente année {{year}}, vous envoie ses voeux de bonheur, de santé et de sérénité.<br>
 
 Nous espérons vous apporter satisfaction et répondre à tous vos besoins futurs.</p>',
   'texttitle' => '{{slogan}}&#8239;!',
-  'maintext' => '<h1>Information accessibilité</h1>
-<p>Si vous souhaitez ouvrir un article dans une nouvelle fenêtre, appuyez sur majuscule plus entrée.<br>
-Ça vous permettra de fermer la fenêtre de l\'article par alt F4.</p><br aria-hidden="true">
-<h1>Présentation</h1><br aria-hidden="true">
+  'maintext' => '<h1>Présentation</h1><br aria-hidden="true">
 <p>Bienvenue sur nael-accessvision.com,<br>
-Vous trouverez des tutoriels audio sur des présentations d\'applications sur téléphone et ordinateur et des démonstrations d\'appareils.<br >
-Vous pourrez aussi trouver des logiciels à télécharger, des astuces en informatique et des retours de tests.</p><br aria-hidden="true">',
+Nous sommes une entreprise engagée dans l’accompagnement des personnes déficientes visuelles, avec pour mission de rendre l’informatique et les outils numériques accessibles à tous.<br>
+Nous proposons des formations sur mesure, adaptées à votre niveau et à vos besoins, que ce soit sur ordinateur, smartphone, ou matériel spécialisé (plages braille, lecteurs d’écran, etc.).<br>
+Notre équipe vous accompagne également dans l’utilisation de vos équipements, la résolution de problèmes techniques, l’aide aux démarches administratives en ligne, et l’autonomie dans les actions du quotidien.<br>
+Nous intervenons auprès des particuliers, des professionnels et des entreprises souhaitant améliorer l’accessibilité de leurs services ou former leurs collaborateurs à l’inclusion numérique.<br>
+Notre approche humaine et personnalisée place vos besoins au centre de nos priorités.<br>
+N’hésitez pas à parcourir nos services pour découvrir tout ce que nous proposons. Pour toute demande spécifique ou projet particulier, contactez-nous, nous serons ravis d’échanger avec vous et de vous accompagner.</p><br aria-hidden="true">',
   'sliderinactext' => '<p>Vous avez choisis de ne pas utiliser notre système d\'informations défilantes, retrouvez donc ci-dessous nos dernières actualités.</p>',
   'sliderinactitle' => 'Actualités',
   'mailconfirmtext' => 'Nous vous remercions pour votre message. Nous y répondrons très bientôt.',

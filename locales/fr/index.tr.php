a7d5cb3cb3f6626606f2b4966c2f0bbaa322a01b
Nael-Sayegh <naelousay@gmail.com>
2025-07-05 20:27:22 +0200

    Update text

--- locales/fr/index.tr.php
+++ locales/fr/index.tr.php
@@ -4,7 +4,7 @@ $tr = [
   '_' => 'index',
   '_todo_level' => 0,
   '_last_author' => 'Pascal',
-  '_last_modif' => 1751739927,
+  '_last_modif' => 1751740042,
   'title' => '{{site}}&nbsp;: {{slogan}}',
   'happynewyear' => '<p role="alert">Toute l\'équipe {{site}} vous souhaite une excellente année {{year}}, vous envoie ses voeux de bonheur, de santé et de sérénité.<br>
 
@@ -14,7 +14,7 @@ Nous espérons vous apporter satisfaction et répondre à tous vos besoins futur
     <p>Bienvenue sur nael-accessvision.com,<br>
     Nous sommes une entreprise engagée dans l’accompagnement des personnes déficientes visuelles, avec pour mission de rendre l’informatique et les outils numériques accessibles à tous.<br>
     Nous proposons des formations sur mesure, adaptées à votre niveau et à vos besoins, que ce soit sur ordinateur, smartphone, ou matériel spécialisé (plages braille, lecteurs d’écran, etc.).<br>
-    Notre équipe vous accompagne également dans l’utilisation de vos équipements, la résolution de problèmes techniques, l’aide aux démarches administratives en ligne, et l’autonomie dans les actions du quotidien.<br>
+    Nous vous accompagnons également dans l’utilisation de vos équipements, la résolution de problèmes techniques, l’aide aux démarches administratives en ligne, et l’autonomie dans les actions du quotidien.<br>
     Nous intervenons auprès des particuliers, des professionnels et des entreprises souhaitant améliorer l’accessibilité de leurs services ou former leurs collaborateurs aux outils numériques.<br>
     Notre approche humaine et personnalisée place vos besoins au centre de nos priorités.<br>
     N’hésitez pas à parcourir nos services pour découvrir tout ce que nous proposons. Pour toute demande spécifique ou projet particulier, contactez-nous, nous serons ravis d’échanger avec vous et de vous accompagner.</p><br aria-hidden="true">',

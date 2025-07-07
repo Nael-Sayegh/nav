78c6e44a5f31e9fbc943a42cd6459c651381dab1
Nael-Sayegh <infos@nael-accessvision.com>
2025-07-07 15:49:54 +0200

    Fix error in index.tr.php

--- locales/fr/index.tr.php
+++ locales/fr/index.tr.php
@@ -4,13 +4,14 @@ $tr = [
   '_' => 'index',
   '_todo_level' => 0,
   '_last_author' => 'Pascal',
-  '_last_modif' => 1751819812,
+  '_last_modif' => 1751896194,
   'title' => '{{site}}&nbsp;: {{slogan}}',
   'happynewyear' => '<p role="alert">Toute l\'équipe {{site}} vous souhaite une excellente année {{year}}, vous envoie ses voeux de bonheur, de santé et de sérénité.<br>
 
 Nous espérons vous apporter satisfaction et répondre à tous vos besoins futurs.</p>',
   'texttitle' => '{{slogan}}&#8239;!',
-  'maintext' => '<h1>Présentation</h1><br aria-hidden="true">
+  'maintext' => <<<ENDSTR
+    <h1>Présentation</h1><br aria-hidden="true">
     <p>Bienvenue sur {{site}},<br>
     Nous sommes une entreprise engagée dans l’accompagnement des personnes déficientes visuelles, avec pour mission de rendre l’informatique et les outils numériques accessibles à tous.<br>
     Nous proposons des formations sur mesure, adaptées à votre niveau et à vos besoins, que ce soit sur ordinateur, smartphone, ou matériel spécialisé (plages braille, lecteurs d’écran, etc.).<br>
@@ -19,7 +20,8 @@ Nous espérons vous apporter satisfaction et répondre à tous vos besoins futur
     Notre approche humaine et personnalisée place vos besoins au centre de nos priorités.<br>
     N’hésitez pas à parcourir nos services pour découvrir tout ce que nous proposons. Pour toute demande spécifique ou projet particulier, contactez-nous, nous serons ravis d’échanger avec vous et de vous accompagner.</p>
     <p>Nous possédons également un blog où vous trouverez des tutoriels, des astuces, des loisirs et lieux accessibles et bien plus encore gratuitement. Pour le consulter c'est par ici&nbsp;:
-    <a href="https://blog.nael-accessvision.com">https://blog.nael-accessvision.com</a></p><br aria-hidden="true">',
+    <a href="https://blog.nael-accessvision.com">https://blog.nael-accessvision.com</a></p><br aria-hidden="true">
+  ENDSTR,,
   'sliderinactext' => '<p>Vous avez choisis de ne pas utiliser notre système d\'informations défilantes, retrouvez donc ci-dessous nos dernières actualités.</p>',
   'sliderinactitle' => 'Actualités',
   'mailconfirmtext' => 'Nous vous remercions pour votre message. Nous y répondrons très bientôt.',

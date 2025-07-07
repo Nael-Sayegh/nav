f7411dc1904cd288449069930d6a601da5b6fff3
Corentin BACQUÉ-CAZENAVE <corentin@progaccess.net>
2025-07-07 17:04:23 +0200

    Fix article deletion

--- admin/sw_mod.php
+++ admin/sw_mod.php
@@ -70,7 +70,7 @@ if ((isset($_GET['token']) && $_GET['token'] === $login['token']) || (isset($_PO
             SQL;
         $reqFiles = $bdd->prepare($SQLFiles);
         $reqFiles->execute(['sw_id' => $_POST['rsw']]);
-        while ($file = $stmtFiles->fetch())
+        while ($file = $reqFiles->fetch())
         {
             $path = $_SERVER['DOCUMENT_ROOT'].'/files/'.$file['hash'];
             if (is_file($path))

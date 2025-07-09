a69b995eda0021f2a99e63079c852c65d71c79c3
Nael-Sayegh <infos@nael-accessvision.com>
2025-07-09 16:49:26 +0200

    Add CGV in footer

--- locales/fr/default.tr.php
+++ locales/fr/default.tr.php
@@ -4,7 +4,7 @@ $tr = [
   '_' => 'default',
   '_todo_level' => 0,
   '_last_author' => 'Nael Sayegh',
-  '_last_modif' => 1751896047,
+  '_last_modif' => 1752072566,
   'slogan' => 'L\'informatique à portée de tous&nbsp;!',
   'banner_homelink' => 'Retour à l\'accueil',
   'fndate' => 'dd/MM/yyyy',
@@ -96,4 +96,5 @@ $tr = [
     <p>{{site}} possède un blog où vous trouverez des tutoriels, des astuces, des loisirs et lieux accessibles  et bien plus encore gratuitement. Pour le consulter c'est par ici&nbsp;:</p>
     <a href="https://blog.nael-accessvision.com">blog.nael-accessvision.com</a>
   ENDSTR,
+  'footer_cgv' => 'Conditions générales de vente',
 ];

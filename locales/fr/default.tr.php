e21fc591d67b8e98a12ce72f7a740aae90b86d28
Nael-Sayegh <infos@nael-accessvision.com>
2025-07-09 18:20:24 +0200

    Add info to open in new tab

--- locales/fr/default.tr.php
+++ locales/fr/default.tr.php
@@ -4,7 +4,7 @@ $tr = [
   '_' => 'default',
   '_todo_level' => 0,
   '_last_author' => 'Nael Sayegh',
-  '_last_modif' => 1752072566,
+  '_last_modif' => 1752078025,
   'slogan' => 'L\'informatique à portée de tous&nbsp;!',
   'banner_homelink' => 'Retour à l\'accueil',
   'fndate' => 'dd/MM/yyyy',
@@ -96,5 +96,5 @@ $tr = [
     <p>{{site}} possède un blog où vous trouverez des tutoriels, des astuces, des loisirs et lieux accessibles  et bien plus encore gratuitement. Pour le consulter c'est par ici&nbsp;:</p>
     <a href="https://blog.nael-accessvision.com">blog.nael-accessvision.com</a>
   ENDSTR,
-  'footer_cgv' => 'Conditions générales de vente',
+  'footer_cgv' => 'Conditions générales de vente (S\'ouvrent dans un nouvel onglet)',
 ];

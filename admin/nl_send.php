d5aabcf2a367b02e3675ef48283e961dc4259665
Corentin BACQUÉ-CAZENAVE <corentin@progaccess.net>
2025-07-16 16:28:24 +0200

    Ability to send manual newsletter

--- admin/nl_send.php
+++ admin/nl_send.php
@@ -21,6 +21,19 @@ if (isset($_GET['act']) && $_GET['act'] === 'form')
     require_once($_SERVER['DOCUMENT_ROOT'].'/tasks/nl_manager.php');
     exit();
 }
+if (isset($_GET['act']) && $_GET['act'] === 'sendnl')
+{
+    $SQL = <<<SQL
+        SELECT * FROM newsletter_mails WHERE confirm=true
+        SQL;
+    $req = $bdd->prepare($SQL);
+    $req->execute();
+    while ($data = $req->fetch())
+    {
+        sendMail($data['mail'], $_POST['obj'], $_POST['text'], "Ce mail est uniquement disponible au format HTML");
+    }
+    exit();
+}
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -32,6 +45,17 @@ if (isset($_GET['act']) && $_GET['act'] === 'form')
 </head>
 <body>
 <?php require_once('include/banner.php'); ?>
+<h2>Envoyer une newsletter aux abonnés de <?php echo $site_name; ?></h2>
+<p>Remplir le formulaire ci-dessous pour envoyer une newsletter à tous les abonnés de <?php echo $site_name; ?></p>
+<form action="?act=sendnl" method="post">
+<label for="f_obj">Sujet&nbsp;:</label>
+<input type="text" name="obj" id="f_obj" required><br>
+<label for="f_text">Texte (HTML)&nbsp;:</label>
+<textarea name="text" id="f_text" maxlength="20000"></textarea><br>
+<input type="submit" value="Envoyer">
+</form>
+<hr>
+<h2>Debuguer l'envoi de la newsletter automatique</h2>
 <form action="?act=form" method="post">
 <label for="maildebug">Debuguer pour&nbsp;:</label>
 <input type="email" name="mail" id="maildebug"><br>

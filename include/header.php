0396d37b6ca257284cf1d4cd1c8777792b929f3e
Nael Sayegh <61361779+Nael-Sayegh@users.noreply.github.com>
2025-07-25 10:54:36 +0200

    Removal of search engine integration from the browser (#13)
    Co-authored-by: Nael-Sayegh <infos@nael-accessvision.com>

--- include/header.php
+++ include/header.php
@@ -3,7 +3,6 @@
 <title><?= $title.' – '.$site_name ?></title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
-<link title="<?= $site_name ?>" type="application/opensearchdescription+xml" rel="search" href="/opensearch.xml.php">
 <?php print $css_path; ?>
 <style>body {font-size: <?php if (isset($_COOKIE['fontsize']) && preg_match('#[0-9]{1,2}#', (string) $_COOKIE['fontsize']))
 {

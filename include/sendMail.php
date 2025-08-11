c5106b841f8c257746a67bef61e8b4fd8621459b
Nael-Sayegh <infos@nael-accessvision.com>
2025-08-11 20:52:15 +0200

    Fix name of logo file

--- include/sendMail.php
+++ include/sendMail.php
@@ -11,7 +11,7 @@ function sendMail($recipients, string $subject, string $contentHtml, string $con
     global $site_name;
     $css = $options['css'] ?? '';
     $includeNotice = $options['includeAutoReplyNotice'] ?? true;
-    $logoUrl = SITE_URL.'/images/logo672-540.png';
+    $logoUrl = SITE_URL.'/images/logo672-450.png';
     $styleBlock = $css
         ? <<<HTML
             <style>

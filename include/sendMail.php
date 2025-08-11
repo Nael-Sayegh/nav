f98f6261d3ba62b36b85fe16378e352ae6c0db4a
Nael-Sayegh <infos@nael-accessvision.com>
2025-08-11 20:56:16 +0200

    Use other logo

--- include/sendMail.php
+++ include/sendMail.php
@@ -11,7 +11,7 @@ function sendMail($recipients, string $subject, string $contentHtml, string $con
     global $site_name;
     $css = $options['css'] ?? '';
     $includeNotice = $options['includeAutoReplyNotice'] ?? true;
-    $logoUrl = SITE_URL.'/images/logo672-450.png';
+    $logoUrl = SITE_URL.'/images/logo1080-1080.png';
     $styleBlock = $css
         ? <<<HTML
             <style>

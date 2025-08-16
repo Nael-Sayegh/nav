ca1a180842c9becc1761008ba7bdfedcce5b56f4
Nael-Sayegh <infos@nael-accessvision.com>
2025-08-16 17:47:02 +0200

    Update unsubscribe text in newsletter

--- admin/nl_send.php
+++ admin/nl_send.php
@@ -59,7 +59,6 @@ if (isset($_GET['act']) && $_GET['act'] === 'sendnl')
     {
         sendMail($email, $_POST['obj'], str_replace('{userid}', $hash, $_POST['text']), 'Ce mail est uniquement disponible au format HTML');
     }
-    exit();
 }
 ?>
 <!DOCTYPE html>
@@ -78,7 +77,7 @@ if (isset($_GET['act']) && $_GET['act'] === 'sendnl')
 <label for="f_obj">Sujet&nbsp;:</label>
 <input type="text" name="obj" id="f_obj" required><br>
 <label for="f_text">Texte (HTML)&nbsp;:</label>
-<textarea name="text" id="f_text" maxlength="20000"><p>Pour vous désinscrire de cette newsletter, cliquez sur le lien suivant&nbsp;: <a href="<?php echo SITE_URL; ?>/nlmod.php?id={userid}"><?php echo SITE_URL; ?>/nlmod.php?id={userid}</a>.</p></textarea><br>
+<textarea name="text" id="f_text" maxlength="20000"><p>Pour vous désinscrire de cette newsletter, cliquez sur le lien suivant&nbsp;: <a href="<?php echo SITE_URL; ?>/nlmod.php?id={userid}">Me désinscrire</a></p></textarea><br>
 <label for="f_site">Envoyer aux abonnés de&nbsp;:</label>
 <select name="site" id="f_site">
 <option value="site1"><?php echo $site_name; ?></option>

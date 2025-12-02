3f32c38b5a6e97982615fdb6f47069a97c161b66
Nael-Sayegh <infos@nael-accessvision.com>
2025-12-02 18:00:25 +0100

    Fix function name in article.php

--- article.php
+++ article.php
@@ -582,7 +582,7 @@ if (isset($logged) && $logged && (checkMemberRights('comment_articles') || ($log
 }*/ ?>
 <fieldset><legend><?= tr($tr, 'comments_send') ?></legend>
 <p><?= tr($tr, 'comments_warn') ?></p>
-<p><?= tr($tr, 'comments_nickname', ['nickname' => getUsernameById($login['id'])]) ?></p>
+<p><?= tr($tr, 'comments_nickname', ['nickname' => $login['usernamek']]) ?></p>
 <label for="fc_text"><?= tr($tr, 'comments_text') ?></label><br>
 <textarea id="fc_text" class="ta" name="text" maxlength="1023" onkeyup="close_confirm=true"><?php /*if (isset($_POST['text']) && strlen((string) $_POST['text']) <= 1023)
 {

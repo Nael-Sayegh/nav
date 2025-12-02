850a8066adf922fdbbeae5871dfb085c2b9315c6
Nael-Sayegh <infos@nael-accessvision.com>
2025-12-02 17:47:52 +0100

    Fix includes in include/banner.php

--- include/banner.php
+++ include/banner.php
@@ -3,9 +3,9 @@
 <?php
 if (isset($_SERVER['HTTP_USER_AGENT']) && str_contains((string) $_SERVER['HTTP_USER_AGENT'], 'Trident'))
 {
-    include __DIR__ . '/include/trident.php';
+    require_once __DIR__ . '/trident.php';
 }
 //include 'include/loginbox.php';
-include __DIR__ . '/include/searchtool.php'; ?>
+require_once __DIR__ . '/searchtool.php'; ?>
 </header>
-<?php include __DIR__ . '/include/menu.php'; ?>
+<?php require_once __DIR__ . '/menu.php'; ?>

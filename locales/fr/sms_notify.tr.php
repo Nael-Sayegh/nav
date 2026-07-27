1748ac3cc2a8037d2065587e0f9091a4a159bb25
Nael-Sayegh <infos@nael-accessvision.com>
2026-07-27 13:48:06 +0200

    Add sms form

---
+++ locales/fr/sms_notify.tr.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+$tr = [
+  '_' => 'sms_notify',
+  '_todo_level' => 0,
+  '_last_author' => 'Nael',
+  '_last_modif' => 1753617600,
+  'title' => 'Recevoir des informations par SMS',
+  'intro' => 'Inscrivez-vous en indiquant votre numéro de téléphone pour recevoir nos informations par SMS.',
+  'label_phone' => 'Numéro de téléphone',
+  'submit' => "S'inscrire",
+  'confirm' => 'Votre inscription a bien été prise en compte.',
+];

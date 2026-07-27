731a434d874126a5818e10cb3b65c0862e03c093
Nael-Sayegh <infos@nael-accessvision.com>
2026-07-27 12:31:58 +0200

    Fix filename for translation

--- locales/fr/privacy.tr.php
+++ locales/fr/privacy.tr.php
@@ -5,28 +5,79 @@ declare(strict_types=1);
 $tr = [
   '_' => 'privacy',
   '_todo_level' => 0,
-  '_last_author' => 'Nael Sayegh',
-  '_last_modif' => 1754926474,
-  'title' => 'politique de confidentialité {{site}}',
-  'maintext' => <<<ENDSTR
-      <h2>{{site}} respecte votre vie privée</h2>
-      <p>Aucune de vos données personnelles (cookies, adresse IP, etc.) n'est transmise à un tiers.<br>
-      Votre adresse IP est conservée temporairement pour des raisons techniques (comptage anonyme des visiteurs, antispam, utilisation de l'espace membre, modification d'un commentaire sur un article), mais ne servira pas à vous identifier.</p>
-      <p>Nous n'utilisons pas et n'utiliserons jamais de méthodes de <a href="https://fr.wikipedia.org/wiki/Empreinte_digitale_d'appareil">fingerprint</a> pour vous identifier contre votre gré.</p>
-
-      <h3>Sécurité</h3>
-      <p>La connexion entre votre ordinateur et {{site}} est sécurisée de bout en bout grâce à un certificat TLS.<br>
-      Cela signifie qu'il est impossible pour un tiers de falsifier les pages web du site ou d'intercepter vos données.<br>
-      Vos mots de passe stockés dans la base de données sont sécurisés de sorte qu'il est impossible, aussi bien pour nous que pour d'éventuels pirates, d'y accéder.</p>
-
-      <h3>Les cookies</h3>
-      <p>Un cookie est une petite donnée déposée dans votre navigateur par un site internet.<br>
-      Vous avez à tout moment la possibilité de bloquer l'usage des cookies ou de les supprimer.<br>
-      Un cookie ne peut aucunement représenter une menace de sécurité.</p>
-      <p>En tout cas, {{site}} ne dépose des cookies dans votre ordinateur qu'avec votre consentement au préalable&nbsp;: un message vous prévient si l'usage des cookies est nécessaire pour utiliser une certaine fonctionnalité.<br>
-      Les cookies utilisés servent exclusivement à la sauvegarde des paramètres de personnalisation et à l'utilisation de l'espace membres {{site}}</p>
-      <h3>En savoir plus</h3>
-      <p>Si vous désirez en savoir plus sur la sécurité de vos données, nos méthodes, votre anonymat, ou toute autre chose, vous pouvez nous contacter via le <a href="/contact.php">page de contact</a> avec le sujet "<i>demande sur la politique de confidentialité</i>".</p>
-      <p>Vous disposez également d'un droit d'accès, de modification et de suppression de vos données, à condition de pouvoir vous identifier comme le propriétaire de ces données.</p>
-      ENDSTR,
+  '_last_author' => 'Nael',
+  '_last_modif' => 1748185111,
+  'title' => 'Politique de confidentialité',
+  'maintext' => '<p>Dernière mise à jour&nbsp: 27 juillet 2026</p>
+<p>La présente Politique de Confidentialité décrit la manière dont Sayegh Nael EI (exploité sous le nom de marque Nael-Accessvision) collecte, utilise, conserve et protège les données à caractère personnel que vous nous fournissez lorsque vous visitez et utilisez notre site web.</p>
+<p>Nous accordons une importance primordiale à la protection de vos données personnelles et nous nous engageons à respecter votre vie privée conformément au Règlement Général sur la Protection des Données (RGPD - Règlement UE 2016/679) et à la Loi Informatique et Libertés.</p>
+<h2>1. Responsable du traitement</h2>
+<p>Le responsable du traitement des données à caractère personnel collectées sur le site est&nbsp:</p>
+<ul>
+<li>Raison sociale&nbsp: Sayegh Nael EI</li>
+<li>Nom de marque&nbsp: Nael-Accessvision</li>
+<li>Forme juridique&nbsp: Entrepreneur Individuel (Micro-entreprise)</li>
+<li>SIRET&nbsp: 940 221 088 00013</li>
+<li>Adresse du siège social&nbsp: 24 rue des Iris, 67400 Illkirch-Graffenstaden, France</li>
+<li>Adresse e-mail de contact&nbsp: <a href="mailto:infos@nael-accessvision.com">infos@nael-accessvision.com</a></li>
+</ul>
+<h2>2. Données personnelles collectées</h2>
+<p>Dans le cadre de l\'exploitation de notre site vitrine et de notre blog, nous sommes amenés à collecter uniquement les données personnelles strictement nécessaires aux services proposés, notamment&nbsp:</p>
+<ul>
+<li>Adresse e-mail (pour l'envoi de la newsletter e-mail)</li>
+<li>Numéro de téléphone portable (pour l'envoi d'informations par SMS)</li>
+<li>Données de connexion et de navigation (adresse IP, type de navigateur, pages consultées) lors de votre visite sur le site.</li>
+</ul>
+<h2>3. Finalités et bases légales du traitement</h2>
+<p>Vos données personnelles sont collectées et traitées pour les finalités et sur les bases légales suivantes&nbsp:</p>
+<table>
+<thead>
+<tr><td>Finalité du traitement</td><td>Base légale du traitement</td></tr>
+</thead>
+<tbody>
+<tr><td>Envoi de la newsletter par e-mail</td><td>Votre consentement préalable</td></tr>
+<tr><td>Envoi d'informations ou d'offres par SMS</td><td>Votre consentement préalable</td></tr>
+<tr><td>Réponse aux demandes de contact ou d\'information</td><td>Intérêt légitime / Mesures précontractuelles</td></tr>
+<tr><td>Assurer la sécurité du site et analyse de navigation</td><td>Intérêt légitime</td></tr>
+</tbody>
+</table>
+<h2>4. Destinataires et sous-traitants des données</h2>
+<p>Vos données à caractère personnel sont strictement confidentielles. Elles ne sont jamais vendues, louées ou cédées à des tiers à des fins commerciales.</p>
+<p>Elles peuvent toutefois être transmises à des prestataires techniques tiers agissant en qualité de sous-traitants au sens du RGPD, uniquement pour la bonne exécution des finalités décrites ci-dessus&nbsp:</p>
+<ul>
+<li>Notre prestataire d'envoi de SMS&nbsp: Spot-Hit (pour le routage et la gestion des campagnes SMS)</li>
+<li>Nos outils techniques internes d\'envoi d\'e-mails&nbsp: pour la diffusion sécurisée de notre newsletter</li>
+</ul>
+<p>Tous nos sous-traitants sont tenus par des obligations strictes de confidentialité et de sécurité conformément au RGPD.</p>
+<h2>5. Transfert de données hors de l\'Union Européenne</h2>
+<p>Vos données personnelles sont stockées et traitées au sein de l\'Union Européenne et ne font l'objet d'aucun transfert vers des pays situés hors de l\'Union Européenne.</p>
+<h2>6. Durée de conservation des données</h2>
+<p>Nous conservons vos données personnelles uniquement pendant la durée nécessaire à la réalisation des finalités pour lesquelles elles ont été collectées&nbsp:</p>
+<ul>
+<li>Données relatives à la newsletter (e-mail et SMS)&nbsp: Conservées tant que vous ne vous désabonnez pas. Un lien de désabonnement (ou une mention STOP SMS) est présent dans chaque communication pour vous permettre de retirer votre consentement à tout moment. En l\'absence d\'interaction de votre part pendant une période de 3 ans, vos données seront supprimées.</li>
+<li>Données de contact&nbsp: Conservées pendant le temps nécessaire au traitement de votre demande, puis archivées ou supprimées au maximum 3 ans après notre dernier contact.</li>
+</ul>
+<h2>7. Vos droits sur vos données personnelles</h2>
+<p>Conformément à la réglementation applicable, vous disposez des droits suivants concernant vos données à caractère personnel &nbsp:</p>
+<ul>
+<li>Droit d\'accès&nbsp: Obtenir la confirmation que vos données sont traitées et en recevoir une copie.</li>
+<li>Droit de rectification&nbsp: Demander la mise à jour ou la correction de données inexactes ou incomplètes.</li>
+<li>Droit à l\'effacement (« Droit à l\'oubli »)&nbsp: Demander la suppression de vos données personnelles.</li>
+<li>Droit de retirer votre consentement&nbsp: Retirer à tout moment votre consentement pour la réception de nos newsletters par e-mail ou SMS.</li>
+<li>Droit à la limitation du traitement&nbsp: Demander le gel temporaire du traitement de vos données.</li>
+<li>Droit d\'opposition&nbsp: Vous opposer à tout moment au traitement de vos données pour des motifs légitimes ou à des fins de prospection commerciale.</li>
+<li>Droit à la portabilité&nbsp: Recevoir vos données dans un format structuré et lisible par machine.</li>
+</ul>
+<h3>Comment exercer vos droits ?</h3>
+<p>Pour exercer l\'un de ces droits, il vous suffit de nous contacter&nbsp:</p>
+<ul>
+<li>Par e-mail à l'adresse&nbsp: <a href="mailto:infos@nael-accessvision.com">infos@nael-accessvision.com</a></li>
+</ul>
+<p>Si vous estimez, après nous avoir contactés, que vos droits ne sont pas respectés, vous pouvez adresser une réclamation auprès de l\'autorité de contrôle compétente en France&nbsp: la CNIL (Commission Nationale de l\'Informatique et des Libertés) sur leur site web&nbsp: <a href="https://www.cnil.fr">www.cnil.fr</a>.</p></p>
+<h2>8. Cookies et traceurs</h2>
+<p>Le site peut utiliser des cookies strictement nécessaires à son bon fonctionnement ainsi que des cookies de mesure d\'audience anonymes. Lorsque l\'usage des cookies est nécessaires, un message d\'information s\'affiche sur le site pour vous informer de leur utilisation. Vous pouvez gérer vos préférences en matière de cookies à tout moment via les paramètres de votre navigateur.</p>
+<h2>9. Sécurité des données</h2>
+<p>Nous mettons en œuvre toutes les mesures techniques et organisationnelles appropriées (chiffrement des données, protocole HTTPS sécurisé, accès restreints) afin de protéger vos données personnelles contre tout accès non autorisé, perte, altération ou divulgation.</p>
+<h2>10. Modification de la politique de confidentialité</h2>
+<p>Nous nous réservons le droit de modifier la présente Politique de Confidentialité à tout moment afin de nous conformer aux évolutions légales et réglementaires. La date de dernière mise à jour sera systématiquement modifiée en haut de la présente page.</p>',
 ];
